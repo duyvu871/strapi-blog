00fae4721b2cdb55634f271f6b1693ae960a63e6
duyvu871 <zoocp123@gmail.com>
2025-04-10 04:12:25 +0700

    add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    apps: [
+        {
+            name: 'strapi-web',
+            script: 'npm run start',
+            out_file: "./front-end-out.log",
+            error_file: "./front-end-error.log",
+            log_date_format: "DD-MM HH:mm:ss Z",
+            env: {
+                NODE_ENV: 'development',
+                PORT: 5000
+            },
+            env_production: {
+                NODE_ENV: 'production',
+                PORT: 5000
+            },
+        },
+    ],
+}
